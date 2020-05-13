be65b6960953593b169b33e4a263083bdb1009e6
Austin Aspaas <araspaas@live.com>
2020-05-13 10:30:17 -0600

    all tests and methods passing

--- lib/room.rb
+++ lib/room.rb
@@ -1,12 +1,25 @@
-class Rom
-
+class Room
+attr_reader :bedroom
   def initialize(category, length, width)
     @category = category
     @length = 10
     @width = width
+    @is_painted = false
+  end
+
+  def category
+    @category = :bedroom
   end
 
   def area
-    @length * @width
+    @length * @width.to_i
+  end
+
+  def is_painted?
+    @is_painted = false
+  end
+
+  def paint
+    @is_painted = true
   end
 end

4a75c70dd0bbbc634f6ba5d7df43c1c26025157d
Austin Aspaas <araspaas@live.com>
2020-05-13 10:58:34 -0600

    I didn't finish iteration 2

--- lib/house.rb
+++ lib/house.rb
@@ -1,9 +1,21 @@
 require "pry"
 
 class House
-  attr_reader :price, :address
+  attr_reader :price,
+              :address,
+              :rooms
+              :room_1
+              :room_2
   def initialize(price, address)
     @price = price
     @address = address
+    @rooms = []
   end
+
+  def add_room(room_1, room_2)
+    @room_1 = room_1
+    @room_2 = room_2
+    @rooms << room_1 && room_2
+  end
+  @rooms
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -19,4 +19,16 @@ class HouseTest < Minitest::Test
     house = House.new("$400000", "123 sugar lane")
     assert_equal "123 sugar lane", house.address
   end
+
+  def test_it_has_rooms
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal [], house.rooms
+  end
+
+  def test_it_can_add_room
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, "13")
+    room_2 = Room.new(:bedroom, 11, "15")
+    assert_equal [room_1, room_2], house.rooms
+  end
 end
